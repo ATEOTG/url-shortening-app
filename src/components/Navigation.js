2f6b5d048000f2bc420f84f3fece378a95d7bc62
ATEOTG <bryansantos9797@gmail.com>
2022-08-05 14:08:41 -0400

    added menu icon and close icon + close/open feature

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,17 +1,40 @@
+import React, { useState } from "react";
 import classes from "./Navigation.module.css";
 import logo from "../assets/images/logo.svg";
 import { RiMenuFill } from "react-icons/ri";
 import { RiCloseLine } from "react-icons/ri";
 
 function Navigation() {
+  const [menuToggle, setMenuToggle] = useState(false);
+
+  function menuToggleHandler() {
+    setMenuToggle((prevState) => !prevState);
+  }
+
+  let menu = (
+    <RiMenuFill
+      onClick={menuToggleHandler}
+      className={classes["menu-icon"]}
+      alt="mobile-menu-icon"
+    />
+  );
+
+  if (menuToggle === true) {
+    menu = (
+      <RiCloseLine
+        onClick={menuToggleHandler}
+        className={classes["close-icon"]}
+        alt="mobile-close-icon"
+      />
+    );
+  }
+
   return (
     <div className={classes.navigation}>
       <div>
         <img src={logo} alt="shortly-logo" />
       </div>
-      <div>
-        <RiMenuFill className={classes["mobile-icon"]} alt="mobile-menu" />
-      </div>
+      <div>{menu}</div>
     </div>
   );
 }
